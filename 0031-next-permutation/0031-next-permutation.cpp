e855836831112410828e76853b64d9b147fc9130
kkm431 <69421853+kkm431@users.noreply.github.com>
2025-05-16 11:37:39 -0400

    Time: 0 ms (100%), Space: 15.6 MB (67.7%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n= nums.size();
-        int i = n-2;
-
-        //Find pivot = nums[i] < nums[i+1] ? nums[i] 
-        while(i >=0 && nums[i] >= nums[i + 1]){
+        int n = nums.size();
+        
+        // Step 1: Find the pivot - the first element from the end that breaks the descending order
+        // We start from the second last element (n-2) and compare with its next element
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-
-        //Find idx from right which is first greater then pivot from right 
+        
+        // Step 2: If we found a pivot (i >= 0), find the smallest element to the right of pivot that's larger than it
         if (i >= 0) {
-            int j = n-1;
-            while(j >=0 && nums[i] >= nums[j]){
+            int j = n - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
                 j--;
             }
-
+            // Swap the pivot with this larger element
             swap(nums[i], nums[j]);
         }
-
+        
+        // Step 3: Reverse the elements after the pivot to get the smallest possible permutation
+        // This works because the elements after pivot were in descending order, 
+        // and reversing makes them ascending (the smallest possible arrangement)
         reverse(nums.begin() + i + 1, nums.end());
+        
+        // The array is now modified to the next permutation in place
     }
 };
